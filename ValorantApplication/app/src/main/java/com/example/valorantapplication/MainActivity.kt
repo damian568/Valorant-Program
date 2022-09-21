c5c5ea2bf5b9d6a741d84c925894b4da33f47def
Дамян Цветков <damian200456@abv.bg>
2022-09-21 20:42:07 +0300

    new

--- ValorantApplication/app/src/main/java/com/example/valorantapplication/MainActivity.kt
+++ ValorantApplication/app/src/main/java/com/example/valorantapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.valorantapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.valorantapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager
+                .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
     }
 }
