f25edc348b8e5dce0aa5a0250fbd8579af5dffbd
Damyan Tsvetkov <didi.milenov@gmail.com>
2023-02-14 14:55:29 +0200

    finished frontend of the quiz

--- ValorantApplication/app/src/main/java/com/example/valorantapplication/fragments/quiz/weapons/FifthQuestionWeaponsFragment.kt
+++ ValorantApplication/app/src/main/java/com/example/valorantapplication/fragments/quiz/weapons/FifthQuestionWeaponsFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.valorantapplication.R
+import androidx.navigation.fragment.findNavController
+import com.example.valorantapplication.databinding.FragmentFifthQuestionWeaponsBinding
 
 class FifthQuestionWeaponsFragment : Fragment() {
+
+    private lateinit var binding: FragmentFifthQuestionWeaponsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_fifth_question_weapons, container, false)
+    ): View {
+        binding = FragmentFifthQuestionWeaponsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sendAnswer()
+    }
+
+    private fun sendAnswer(){
+        binding.sendFifthAnswerWeapons.setOnClickListener {
+            goToNextQuestion()
+        }
+    }
+
+    private fun goToNextQuestion(){
+        val action =
+            FifthQuestionWeaponsFragmentDirections.actionFifthQuestionWeaponsFragmentToEndOfQuizFragment()
+        findNavController().navigate(action)
     }
 }
